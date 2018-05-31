a3eba0c240ce51b0f528b9217126fe4758be4ee7
Sonali Kshirsagar <sonali@kmac.attlocal.net>
2018-05-31 10:56:28 -0700

    Fixed checkstyle errors

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,8 +12,8 @@ public class Greeter {
 
   }
 
-  //TODO: Add javadoc comment
-  public String greet(String someone) {
+  /This is a new comment in javadoc comment
+  public final String greet(String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
